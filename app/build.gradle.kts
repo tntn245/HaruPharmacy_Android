bf938aa1342071dd109477485638e48ddf7b89d7
Huyz04 <huyhuyhuy.as.vui@gmail.com>
2024-04-11 01:16:02 +0700

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("com.intuit.sdp:sdp-android:1.0.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.pharmacyandroidapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.product_detail);
+        LinearLayout Detail = findViewById(R.id.detail_product);
+
     }
 }
