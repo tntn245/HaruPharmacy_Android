c3fcf9714b08c2cedfde8f97910b6e634a5816c7
tntn245 <tranngoctonhu2405@gmail.com>
2024-04-14 16:12:00 +0700

    FE warehouse management (home)

---
+++ app/src/main/java/com/example/pharmacyandroidapplication/WarehouseManagementActivity.java
@@ -0,0 +1,14 @@
+package com.example.pharmacyandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WarehouseManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warehouse_management);
+    }
+}
