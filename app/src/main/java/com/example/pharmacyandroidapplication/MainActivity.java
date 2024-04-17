94d227205b1f0093cf6432eaab5ed6d01741418c
tntn245 <tranngoctonhu2405@gmail.com>
2024-04-17 21:57:10 +0700

    test code run layout StockInDetails

--- app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
@@ -16,15 +16,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wh_stock_in_details);
-        GridView StockInWH= findViewById(R.id.list_product_stock_in);
-        ArrayList<StockIn> StockArrayList = new ArrayList<StockIn>();
-
-        StockArrayList.add(new StockIn(new Date(2024, 4, 2), 100000));
-        StockArrayList.add(new StockIn(new Date(2024, 4, 1),100000));
-        StockArrayList.add(new StockIn(new Date(2024, 3, 29),100000));
-        StockArrayList.add(new StockIn(new Date(2024, 3, 18),100000));
-
-        StockInAdapter adapter = new StockInAdapter(this, StockArrayList);
-        StockInWH.setAdapter(adapter);
     }
 }
