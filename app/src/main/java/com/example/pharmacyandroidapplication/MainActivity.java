b72ccd496f1d75c67a809add7df8ba889cb20215
tntn245 <tranngoctonhu2405@gmail.com>
2024-04-17 21:52:56 +0700

    pull from main

--- app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
@@ -4,8 +4,7 @@ import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.pharmacyandroidapplication.adapters.ProductStockInDetailsAdapter;
-import com.example.pharmacyandroidapplication.models.ProductStockInDetails;
+import com.example.pharmacyandroidapplication.adapters.StockInAdapter;
 import com.example.pharmacyandroidapplication.models.StockIn;
 
 import java.util.ArrayList;
@@ -16,15 +15,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wh_stock_in_details);
-        GridView StockInDetails= findViewById(R.id.list_product_stock_in);
-        ArrayList<ProductStockInDetails> productStockInDetails = new ArrayList<ProductStockInDetails>();
-
-        productStockInDetails.add(new ProductStockInDetails("Chromium","SDGDSE",new Date(2023,1,1),new Date(2024, 4, 2), 10,100000,R.drawable.pro1));
-        productStockInDetails.add(new ProductStockInDetails("Omega3","UDXFDG",new Date(2023,1,1),new Date(2024, 4, 1),10,100000,R.drawable.pro2));
-        productStockInDetails.add(new ProductStockInDetails("Thyroid-Pro Formula","DGBDFS",new Date(2023,1,1),new Date(2024, 3, 29),10,100000,R.drawable.pro3));
-
-        ProductStockInDetailsAdapter adapter = new ProductStockInDetailsAdapter(this, productStockInDetails);
-        StockInDetails.setAdapter(adapter);
+        setContentView(R.layout.activity_address_input);
     }
 }
---
+++ app/src/main/java/com/example/pharmacyandroidapplication/activities/admin/WarehouseStockInDetails.java
@@ -0,0 +1,2 @@
+package com.example.pharmacyandroidapplication.activities.admin;public class WarehouseStockInDetails {
+}
