bbc6dca5345248f8af2d0264431f051d2308ec3a
tntn245 <tranngoctonhu2405@gmail.com>
2024-04-23 22:31:31 +0700

    register (not done)

--- app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/MainActivity.java
@@ -1,20 +1,50 @@
 package com.example.pharmacyandroidapplication;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pharmacyandroidapplication.adapters.StockInAdapter;
 import com.example.pharmacyandroidapplication.models.StockIn;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText editTextEmail, editTextPassword;
+    Button buttonReg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_address_input);
+        super. onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        editTextEmail = findViewById(R.id.usernameEditText);
+        editTextPassword = findViewById(R.id.passwordEditText);
+        buttonReg = findViewById(R.id.SignUpButton);
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils.isEmpty(email) ){
+                    Toast.makeText(MainActivity.this,"Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password) ) {
+                    Toast.makeText(MainActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 }
