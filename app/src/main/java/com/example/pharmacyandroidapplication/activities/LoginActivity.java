35d0676e74c3ffa03196a9e229d44d3157e01fdc
tntn245 <tranngoctonhu2405@gmail.com>
2024-04-24 21:35:27 +0700

    fix FE customer homepage

--- app/src/main/java/com/example/pharmacyandroidapplication/activities/LoginActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/activities/LoginActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.pharmacyandroidapplication.MainActivity;
 import com.example.pharmacyandroidapplication.R;
+import com.example.pharmacyandroidapplication.activities.customer.ForgotPassActivity;
+import com.example.pharmacyandroidapplication.activities.customer.SignUpActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,11 +20,20 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        TextView textViewRegister = findViewById(R.id.textViewRegister);
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView textViewForgotPass = findViewById(R.id.textViewForgotPass);
+        textViewForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPassActivity.class);
                 startActivity(intent);
             }
         });
