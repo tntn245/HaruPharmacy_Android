76aba9ad5dcf97fe5ef1e3e95ec8fa57a919c365
tntn245 <tranngoctonhu2405@gmail.com>
2024-04-24 21:58:26 +0700

    click on bottom nav

--- app/src/main/java/com/example/pharmacyandroidapplication/activities/customer/CustomerHomepageActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/activities/customer/CustomerHomepageActivity.java
@@ -1,15 +1,23 @@
 package com.example.pharmacyandroidapplication.activities.customer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pharmacyandroidapplication.MainActivity;
 import com.example.pharmacyandroidapplication.R;
+import com.example.pharmacyandroidapplication.activities.ChatActivity;
+import com.example.pharmacyandroidapplication.activities.LoginActivity;
 import com.example.pharmacyandroidapplication.adapters.HomeCategoryAdapter;
 import com.example.pharmacyandroidapplication.adapters.HomeProductAdapter;
 import com.example.pharmacyandroidapplication.models.Category;
 import com.example.pharmacyandroidapplication.models.Product;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -37,6 +45,31 @@ public class CustomerHomepageActivity extends AppCompatActivity {
 
         HomeProductAdapter productAdapter = new HomeProductAdapter(this, ProductArrayList);
         productGV.setAdapter(productAdapter);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    // Xử lý khi người dùng chọn trang chủ
+                    return true;
+                } else if (itemId == R.id.support) {
+                    // Xử lý khi người dùng chọn trang tư vấn
+                    Intent supportIntent = new Intent(CustomerHomepageActivity.this, ChatActivity.class);
+                    startActivity(supportIntent);
+                    return true;
+                } else if (itemId == R.id.cart) {
+                    // Xử lý khi người dùng chọn trang giỏ hàng
+                    return true;
+                } else if (itemId == R.id.profile) {
+                    // Xử lý khi người dùng chọn trang tài khoản
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 }
