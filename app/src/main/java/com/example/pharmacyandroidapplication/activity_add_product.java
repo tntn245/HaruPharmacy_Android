9cf48032540dc00278d32af818307c99f8c4c21e
LeThuyDuong <duonglthuy.03@gmail.com>
2024-05-01 14:47:05 +0700

    add/delete product/category

---
+++ app/src/main/java/com/example/pharmacyandroidapplication/activity_add_product.java
@@ -0,0 +1,14 @@
+package com.example.pharmacyandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_add_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
