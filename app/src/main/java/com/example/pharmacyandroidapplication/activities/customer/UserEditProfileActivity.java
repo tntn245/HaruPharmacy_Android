cffafcd966a4f1eded4d6925ceff00376fc8e62d
Duykhongkhoc <21521998@gm.uit.edu.vn>
2024-05-12 01:38:14 +0700

    Feature add edit profile (not firebase yet)

--- app/src/main/java/com/example/pharmacyandroidapplication/activities/customer/UserEditProfileActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/activities/customer/UserEditProfileActivity.java
@@ -1,7 +1,10 @@
 package com.example.pharmacyandroidapplication.activities.customer;
 
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,16 +15,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.pharmacyandroidapplication.R;
 
 public class UserEditProfileActivity extends AppCompatActivity {
-    private TextView txt_acc_birth_day, txt_acc_sex;
     private EditText edtTxt_edit_acc_birth_day, edtTxt_edit_acc_sex;
-    Button saveBtn = findViewById(R.id.btn_edit_acc_info);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-        txt_acc_birth_day = findViewById(R.id.txt_acc_birth_day);
-
+        edtTxt_edit_acc_birth_day = findViewById(R.id.edtTxt_edit_acc_birth_day);
+        edtTxt_edit_acc_sex = findViewById(R.id.edtTxt_edit_acc_sex);
+        Button saveBtn = findViewById(R.id.btn_saved_edit_acc_info);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveProfile();
+            }
+        });
+    }
+    public void saveProfile(){
+        String sex= edtTxt_edit_acc_sex.getText().toString();
+        String birth_day = edtTxt_edit_acc_birth_day.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra("sex", sex);
+        intent.putExtra("birthDay", birth_day);
+        // Đặt resultCode và Intent và kết thúc hoạt động
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
 }
--- app/src/main/java/com/example/pharmacyandroidapplication/activities/customer/UserProfileActivity.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/activities/customer/UserProfileActivity.java
@@ -1,14 +1,44 @@
 package com.example.pharmacyandroidapplication.activities.customer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pharmacyandroidapplication.R;
 
 public class UserProfileActivity extends AppCompatActivity {
+    private static final int EDIT_PROFILE_REQUEST_CODE = 1001;
+    private TextView txt_acc_birth_day, txt_acc_sex;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        Button btnEditProfile = findViewById(R.id.btn_edit_acc_info);
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfileActivity.this, UserEditProfileActivity.class);
+                startActivityForResult(intent, EDIT_PROFILE_REQUEST_CODE);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EDIT_PROFILE_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                txt_acc_birth_day = findViewById(R.id.txt_acc_birth_day);
+                txt_acc_sex = findViewById(R.id.txt_acc_sex);
+                String sex = data.getStringExtra("sex");
+                String birthDay = data.getStringExtra("birthDay");
+                txt_acc_sex.setText(sex);
+                txt_acc_birth_day.setText(birthDay);
+            }
+        }
     }
 }
