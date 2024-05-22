a0f0db4d8a1a1dd6d51daa677f4ab2cc9a42850c
Duykhongkhoc <21521998@gm.uit.edu.vn>
2024-05-23 05:58:36 +0700

    Feature Order Tracking and Order Details

--- app/src/main/java/com/example/pharmacyandroidapplication/adapters/ShipmentInfAdapter.java
+++ app/src/main/java/com/example/pharmacyandroidapplication/adapters/ShipmentInfAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pharmacyandroidapplication.R;
 import com.example.pharmacyandroidapplication.models.ShipmentInf;
@@ -15,10 +18,27 @@ import java.util.List;
 public class ShipmentInfAdapter extends BaseAdapter {
     private Context context;
     private List<ShipmentInf> ShipmentInfList;
+    private OnDeleteClickListener deleteClickListener;
+    private OnEditClickListener editClickListener;
 
     public ShipmentInfAdapter(Context context, List<ShipmentInf> ShipmentInfList) {
         this.context = context;
         this.ShipmentInfList = ShipmentInfList;
+    }
+    public interface OnDeleteClickListener {
+        void onDeleteClick(String addressId);
+    }
+
+    public void setOnDeleteClickListener(OnDeleteClickListener listener) {
+        this.deleteClickListener = listener;
+    }
+
+    public interface OnEditClickListener {
+        void onEditClick(ShipmentInf shipmentInf);
+    }
+
+    public void setOnEditClickListener(OnEditClickListener listener) {
+        this.editClickListener = listener;
     }
 
     @Override
@@ -38,20 +58,51 @@ public class ShipmentInfAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.item_address, parent, false);
+            holder = new ViewHolder();
+            holder.img_delete_address_item = convertView.findViewById(R.id.img_delete_address_item);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
-
-        ShipmentInf ShipmentInf = ShipmentInfList.get(position);
+        final ShipmentInf shipmentInf = ShipmentInfList.get(position);
 
         TextView txtName = convertView.findViewById(R.id.txt_name_receiver_address);
         TextView txtPhone = convertView.findViewById(R.id.txt_phone_address);
         TextView txtAddress = convertView.findViewById(R.id.txt_total_address_address);
 
-        txtName.setText(ShipmentInf.getReceiverName());
-        txtPhone.setText(ShipmentInf.getPhone());
-        txtAddress.setText(ShipmentInf.getAddress_details() + ", " + ShipmentInf.getCommune() + ", " + ShipmentInf.getDistrict() + ", " + ShipmentInf.getProvince());
+        txtName.setText(shipmentInf.getReceiverName());
+        txtPhone.setText(shipmentInf.getPhone());
+        txtAddress.setText(shipmentInf.getAddress_details() + ", " + shipmentInf.getCommune() + ", " + shipmentInf.getDistrict() + ", " + shipmentInf.getProvince());
+        //Xử lý sự kiện Xóa
+        ImageView imgDeleteAddress = convertView.findViewById(R.id.img_delete_address_item);
+        imgDeleteAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy ra address_id của địa chỉ
+                String addressId = ShipmentInfList.get(position).getAddress_id();
 
+                // Gọi phương thức xử lý sự kiện khi bấm vào img_delete_address
+                if (deleteClickListener != null) {
+                    deleteClickListener.onDeleteClick(addressId);
+                }
+            }
+        });
+        ImageView img_edit_address = convertView.findViewById(R.id.img_edit_address_item);
+        img_edit_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editClickListener != null) {
+                    editClickListener.onEditClick(shipmentInf);
+                }
+            }
+        });
         return convertView;
     }
+    private static class ViewHolder {
+        ImageView img_delete_address_item;
+    }
 }
+
